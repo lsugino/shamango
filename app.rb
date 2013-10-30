47afe934ddac1f694c7b800a459165e120b29a0f
Daniel Earthy <danielearthy@gmail.com>
2013-10-29 18:44:55 -0700

    stuff

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require 'sinatra'
 require 'active_record'
 require_relative './app/models/member'
+require_relative './app/models/post'
 
 
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
                                         database: 'shamango')
 
 get '/' do
-   erb :index  
+  erb :index  
+end
+
+get '/create' do
+  erb :create_user
+end
+
+post '/create_user' do
+  member = Member.create 
+  p params
 end
