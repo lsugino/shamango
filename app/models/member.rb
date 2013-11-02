daef4bf4f5b50d8f53e1b0e0b554c6092924984a
Apprentice <apprentice@dbc-04.local>
2013-11-01 18:28:14 -0700

    implement password hashing feature

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,20 @@
+require 'bcrypt'
+
 class Member < ActiveRecord::Base
   has_many :posts
   def name
   	"#{self.first_name.split.join}#{self.last_name.split.join}"
+  end
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
   end
 
 end
